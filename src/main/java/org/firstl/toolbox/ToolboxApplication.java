e30e2f59a61222219d500d3b79e0ef12effd90e9
Hermann K <n-guessan-hermann.kouadio@etudiant.univ-rennes1.fr>
2021-04-08 16:47:56 +0200

    Initial commit

---
+++ src/main/java/org/firstl/toolbox/ToolboxApplication.java
@@ -0,0 +1,21 @@
+package org.firstl.toolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ToolboxApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolboxApplication.class, args);
+	}
+
+}
